d7c3b7839ca85e8f38d3a6151776bd26028437bd
David Eklund <deklund@gmail.com>
2020-06-25 20:48:40 -0400

    Clean-up housing101.js

--- src/pages/housing101.js
+++ src/pages/housing101.js
@@ -8,13 +8,15 @@ export default () => (
                 <h3 class="card-title">Affordable Housing 101</h3>
                 <p className="card-text">
                     <Row>
-                    <Col>To address the housing crisis through regional
-                    collaboration and mutual support, the Metropolitan
-                    Mayors Coalition (MMC) launched a Regional
-                    Housing Taskforce to establish a regional housing
-                    production goal and identify strategies to achieve
-                    that goal throughout the 15 participating
-                        communities.</Col>
+                        <Col>
+                            To address the housing crisis through regional
+                            collaboration and mutual support, the Metropolitan
+                            Mayors Coalition (MMC) launched a Regional
+                            Housing Taskforce to establish a regional housing
+                            production goal and identify strategies to achieve
+                            that goal throughout the 15 participating
+                            communities.
+                        </Col>
                         <Col>Image here</Col>
                     </Row>
                 </p>
