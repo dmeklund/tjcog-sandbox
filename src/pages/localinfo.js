55136ce26b47a8e4d6f2622c00a956173cf58b81
David Eklund <deklund@gmail.com>
2020-07-14 23:19:20 -0400

    Add ArcGIS sample to local info page

---
+++ src/pages/localinfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import {WebMapView} from "../components/mapview"
+
+const LocalInfo = () => (
+    <Container>
+        <WebMapView />
+    </Container>
+)
+
+export default LocalInfo;
