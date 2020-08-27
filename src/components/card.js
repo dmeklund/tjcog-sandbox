83526194f2f5c4efdff632b2781b850aeb04fac2
Brian Torres <brian.al.torres@gmail.com>
2020-08-26 22:16:13 -0400

    updated card

--- src/components/card.js
+++ src/components/card.js
@@ -1,19 +1,24 @@
-import React, { Children } from "react";
-import {Col, Container, Row} from "react-bootstrap";
+import React, { Component } from "react";
+import {Container} from "react-bootstrap";
 import "./card.css"
+import "./cardCol.css"
 
-export const Card = props => (
-  <Container fluid>
-      <div class="card" style={{width: "100%"}}>
-        <div class="card-body">
-          <Row class="cardRow">
-            <Col class="cardCol">
-              <h1 class="card-title">{props.title}</h1>
-              <p class="card-text">{props.children}</p>
-            </Col>
-            <Col><img src={props.imgsrc} class="card-img" alt="..."/></Col>
-          </Row>
-        </div>
-      </div>
-  </Container>
+export const CardCol =  ({children}) => (
+    <div class="card-content">{children}</div>
 );
+
+export default class Card extends Component {
+    static Col = CardCol;
+
+    render() {
+        return (
+            <Container className="card-container">
+                <div class="card">
+                    <div class="card-body">
+                        {this.props.children}
+                    </div>
+                </div>
+            </Container>
+        )
+    }
+};
