da4fa62c8d7916084eaf3d265dc4edd688e5644a
Spencer Kline <dskline1@gmail.com>
2020-09-07 09:30:28 -0400

    Add and update page names to match the latest wireframes
    Resolves #7

---
+++ src/pages/glossary.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+const Glossary = () => (
+    <Container>
+        <h1>Glossary of Terms</h1>
+        <p>This is the Glossary of Terms page</p>
+    </Container>
+)
+
+export default Glossary;
