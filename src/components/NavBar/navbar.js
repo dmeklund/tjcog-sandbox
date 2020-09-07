dc64ea23fe572e5dad52af7a6b2d9a2335c6dfe9
Spencer Kline <dskline1@gmail.com>
2020-09-07 09:42:49 -0400

    Have links navigate without refreshing the page

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -2,35 +2,43 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Image from "react-bootstrap/Image";
+import { Link } from "@reach/router";
+
 import { NavItems } from "./navbarItems";
 import { NavbarMenu } from "./navbarMenu";
 
 export default () => (
   <div className="d-flex flex-column d-lg-block">
     <Nav className="justify-content-between p-2">
-      <Navbar.Brand href="/" className="d-none d-lg-flex pl-2">
-        <Image height={90} width={90} src="/logo.png" />
-        <div className="d-flex-col m-auto pl-2 text-uppercase">
-          <div className="h2 mb-0" style={{ fontWeight: 800 }}>
-            Triangle J
+      <Link to="/">
+        <Navbar.Brand className="d-none d-lg-flex pl-2">
+          <Image height={90} width={90} src="/logo.png" />
+          <div className="d-flex-col m-auto pl-2 text-uppercase">
+            <div className="h2 mb-0" style={{ fontWeight: 800 }}>
+              Triangle J
+            </div>
+            <div>Council of Governments</div>
           </div>
-          <div>Council of Governments</div>
-        </div>
-      </Navbar.Brand>
-      <Navbar.Brand href="/" className="d-flex d-lg-none pl-2">
-        <Image height={40} width={40} src="/logo.png" />
-        <div className="d-flex-col m-auto pl-2 text-uppercase">
-          <div className="h4 mb-0" style={{ fontWeight: 800 }}>
-            Triangle J
+        </Navbar.Brand>
+        <Navbar.Brand className="d-flex d-lg-none pl-2">
+          <Image height={40} width={40} src="/logo.png" />
+          <div className="d-flex-col m-auto pl-2 text-uppercase">
+            <div className="h4 mb-0" style={{ fontWeight: 800 }}>
+              Triangle J
+            </div>
+            <div style={{ fontSize: "0.8rem" }}>Council of Governments</div>
           </div>
-          <div style={{ fontSize: "0.8rem" }}>Council of Governments</div>
-        </div>
-      </Navbar.Brand>
+        </Navbar.Brand>
+      </Link>
       <div>
         <Nav className="align-items-center">
           <div className="d-none d-md-flex">
-            <Nav.Link href="/about">About Us</Nav.Link>
-            <Nav.Link href="/contact">Contact Us</Nav.Link>
+            <Link to="/about" className="pr-4">
+              About Us
+            </Link>
+            <Link to="/contact" className="pr-4">
+              Contact Us
+            </Link>
           </div>
           <NavbarMenu />
         </Nav>
--- src/components/NavBar/navbarItems.js
+++ src/components/NavBar/navbarItems.js
@@ -1,21 +1,22 @@
-import Navbar from "react-bootstrap/Navbar";
 import React from "react";
+import { Link } from "@reach/router";
+import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
 export const NavItems = (props) => (
   <Navbar bg="primary" {...props}>
-    <NavItem href="/">Affordable Housing 101</NavItem>
-    <NavItem href="/triangle-regional-data">Triangle Regional Data</NavItem>
-    <NavItem href="/strategies">Best Practices & Strategies</NavItem>
-    <NavItem href="/resources">Resources</NavItem>
-    <NavItem href="/glossary">Glossary of Terms</NavItem>
-    <NavItem href="/faq">FAQ</NavItem>
+    <NavItem to="/">Affordable Housing 101</NavItem>
+    <NavItem to="/triangle-regional-data">Triangle Regional Data</NavItem>
+    <NavItem to="/strategies">Best Practices & Strategies</NavItem>
+    <NavItem to="/resources">Resources</NavItem>
+    <NavItem to="/glossary">Glossary of Terms</NavItem>
+    <NavItem to="/faq">FAQ</NavItem>
 
     {/* These items are removed on small screens, so we add them to the dropdown menu */}
-    <NavItem href="/about" className="d-flex d-md-none">
+    <NavItem to="/about" className="d-flex d-md-none">
       About Us
     </NavItem>
-    <NavItem href="/contact" className="d-flex d-md-none">
+    <NavItem to="/contact" className="d-flex d-md-none">
       Contact Us
     </NavItem>
   </Navbar>
@@ -25,7 +26,7 @@ const NavItem = (props) => {
   const { className, ...rest } = props;
   return (
     <Nav.Item className={`m-auto text-center ${className || ""}`}>
-      <Nav.Link className="text-white font-weight-bold" {...rest} />
+      <Link className="text-white font-weight-bold" {...rest} />
     </Nav.Item>
   );
 };
