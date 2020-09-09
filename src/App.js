abbfaa27ee173008f98edac5d094cec80c0f8763
Kevin Muenkel <kevin_tm@yahoo.com>
2020-09-09 01:06:25 -0400

    corrected missing imports

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import { Router } from "@reach/router";
 import "./App.css";
 import Layout from "./components/layout";
 import About from "./pages/about";
+import TriangleRegionalData from "./pages/triangleRegionalData";
+import Strategies from "./pages/strategies";
+import LocalInfo from "./pages/localinfo";
+import Resources from "./pages/resources";
+import Glossary from "./pages/glossary";
+import FAQ from "./pages/faq";
+import Contact from "./pages/contact";
+import Housing from "./pages/housing101"
 
 function App() {
   return (
