2f94a8c9b39ac053b76f5b5d35de1654978ff162
Kevin Muenkel <kevin_tm@yahoo.com>
2020-09-09 02:36:12 -0400

    elaborated demo page

--- src/pages/housing101.js
+++ src/pages/housing101.js
@@ -1,12 +1,14 @@
 import React from "react";
 import {Col, Container, Row} from "react-bootstrap";
+import Tabs from "../components/Tabs";
+import ControlledAccordions from "./accordion-demo";
+import LocalInfo from "./localinfo";
 
 export default () => (
     <Container>
-        <div class="card" style={{width: "100%"}}>
-            <div class="card-body">
-                <h3 class="card-title">Affordable Housing 101</h3>
-                <p className="card-text">
+        <div className="card" style={{width: "100%"}}>
+            <div className="card-body">
+                <h3 className="card-title">Affordable Housing 101</h3>
                     <Row>
                         <Col>
                             To address the housing crisis through regional
@@ -19,7 +21,33 @@ export default () => (
                         </Col>
                         <Col>Image here</Col>
                     </Row>
-                </p>
+
+                    <Row>
+                        <Tabs className="style2">
+                            <Tabs.Pane title="Demographics">
+                                <Row>
+                                    <Col>
+                                        <ControlledAccordions />
+                                    </Col>
+                                    <Col style={{maxHeight: "300px", overflow: "auto"}}>
+                                        <LocalInfo />
+                                    </Col>
+                                </Row>
+                            </Tabs.Pane>
+                            <Tabs.Pane title="Housing Stock">
+                                Housing Stock
+                            </Tabs.Pane>
+                            <Tabs.Pane title="Cost-Burdened Households">
+                                Cost-Burdened Households
+                            </Tabs.Pane>
+                            <Tabs.Pane title="Development & Land Use">
+                                Development & Land Use
+                            </Tabs.Pane>
+                            <Tabs.Pane title="Housing Market Conditions">
+                                Housing Market Conditions
+                            </Tabs.Pane>
+                        </Tabs>
+                    </Row>
             </div>
         </div>
     </Container>
